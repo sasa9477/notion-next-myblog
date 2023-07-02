ee1ffe22526d163d40585edb98d0b26d061e12a2
sasa9477 <sasa9477@outlook.com>
2023-07-03 01:42:20 +0900

    taiwindcssから unocssに変更

---
+++ uno.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
+
+export default defineConfig({
+  presets: [
+    presetUno({
+      dark: 'media',
+    }),
+    presetAttributify(),
+    presetIcons({
+      extraProperties: {
+        display: 'inline-block',
+        'vertical-align': 'middle',
+      },
+    }),
+  ],
+})
